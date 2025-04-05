c86cb374e593fdc4f3a638bce2c4ea044fc85e97
Andrea <andrew.ryan.r.a@gmail.com>
2025-04-05 18:55:21 +0200

    Back Office: added products list

--- scripts/back-office.js
+++ scripts/back-office.js
@@ -1,7 +1,13 @@
 'use strict';
 
-const form = document.querySelector('form');
+const PRODUCTS_API = 'https://striveschool-api.herokuapp.com/api/product/';
+const ACCESS_TOKEN =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2RmYzUzNmQyZWM1YzAwMTUzOTM4MGUiLCJpYXQiOjE3NDM2MTQ2MTYsImV4cCI6MTc0NDgyNDIxNn0.7n77yIzfN7AcDP7EsmembYT-UEcqHExS9W084_8ZYaw';
 
+const tableBody = document.getElementById('tableBody');
+const addProductForm = document.getElementById('addProductForm');
+
+// FORM: ADD NEW PRODUCT
 const addNewProduct = async payload => {
     try {
         const response = await fetch(PRODUCTS_API, {
@@ -20,19 +26,52 @@ const addNewProduct = async payload => {
 
 const getFormValues = () => {
     const payload = {
-        brand: form.brand.value,
-        name: form.productName.value,
-        imageUrl: form.imageUrl.value,
-        description: form.description.value,
-        price: Number(form.price.value),
+        brand: addProductForm.brand.value,
+        name: addProductForm.productName.value,
+        imageUrl: addProductForm.imageUrl.value,
+        description: addProductForm.description.value,
+        price: Number(addProductForm.price.value),
     };
 
     return payload;
 };
 
-form.addEventListener('submit', async event => {
+addProductForm.addEventListener('submit', async event => {
     event.preventDefault();
     const payload = getFormValues();
 
     await addNewProduct(payload).then(product => console.log(product));
 });
+
+// PRODUCTS LIST
+const getProducts = async () => {
+    try {
+        const response = await fetch(PRODUCTS_API, {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${ACCESS_TOKEN}`,
+            },
+        });
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const generateTableRow = data => {
+    const { brand, name, imageUrl, description, price } = data;
+    const tr = document.createElement('tr');
+
+    [brand, name, imageUrl, description, price].forEach(item => {
+        const td = document.createElement('td');
+        td.setAttribute('class', 'table-body-cell');
+        td.textContent = item;
+
+        tr.appendChild(td);
+    });
+    tableBody.appendChild(tr);
+};
+
+getProducts().then(products =>
+    products.forEach(product => generateTableRow(product))
+);
